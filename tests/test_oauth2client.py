53ec0d4cf017e8f4ef4f180f105a20cf19c14719
Aaron <aaronwinter@users.noreply.github.com>
2015-01-26 15:26:10 -0500

    Add OpenDirector instance to the list of arguments passed to open

--- tests/test_oauth2client.py
+++ tests/test_oauth2client.py
@@ -215,7 +215,7 @@ class GoogleCredentialsTests(unittest.TestCase):
                            return_value=response,
                            autospec=True) as open:
       self.assertEqual('GCE_PRODUCTION', _get_environment())
-      no_proxy_conf.open.assert_called_once_with('http://169.254.169.254/', timeout=1)
+      open.assert_called_once_with(no_proxy_conf, 'http://169.254.169.254/', timeout=1)
 
   def test_get_environment_unknown(self):
     os.environ['SERVER_SOFTWARE'] = ''
@@ -225,7 +225,7 @@ class GoogleCredentialsTests(unittest.TestCase):
                            return_value=response,
                            autospec=True) as open:
       self.assertEqual(DEFAULT_ENV_NAME, _get_environment())
-      no_proxy_conf.open.assert_called_once_with('http://169.254.169.254/', timeout=1)
+      open.assert_called_once_with(no_proxy_conf, 'http://169.254.169.254/', timeout=1)
 
   def test_get_environment_variable_file(self):
     environment_variable_file = datafile(
