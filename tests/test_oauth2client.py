04c432c0d0d0c48b4b23a0e7faf4014ca7b5f0ae
Aaron <aaronwinter@users.noreply.github.com>
2015-01-27 01:58:35 -0500

    Update argument in unit tests to fit recent changes made in GCE environment detection

--- tests/test_oauth2client.py
+++ tests/test_oauth2client.py
@@ -209,21 +209,23 @@ class GoogleCredentialsTests(unittest.TestCase):
 
   def test_get_environment_gce_production(self):
     os.environ['SERVER_SOFTWARE'] = ''
+    no_proxy_conf = urllib.request.build_opener(urllib.request.ProxyHandler({}))
     response = MockResponse({'Metadata-Flavor': 'Google'})
-    with mock.patch.object(urllib.request, 'urlopen',
+    with mock.patch.object(no_proxy_conf, 'open',
                            return_value=response,
-                           autospec=True) as urlopen:
+                           autospec=True) as open:
       self.assertEqual('GCE_PRODUCTION', _get_environment())
-      urlopen.assert_called_once_with(
+      no_proxy_conf.open.assert_called_once_with(
           'http://169.254.169.254/', timeout=1)
 
   def test_get_environment_unknown(self):
     os.environ['SERVER_SOFTWARE'] = ''
-    with mock.patch.object(urllib.request, 'urlopen',
+    no_proxy_conf = urllib.request.build_opener(urllib.request.ProxyHandler({}))
+    with mock.patch.object(no_proxy_conf, 'open',
                            return_value=MockResponse({}),
-                           autospec=True) as urlopen:
+                           autospec=True) as open:
       self.assertEqual(DEFAULT_ENV_NAME, _get_environment())
-      urlopen.assert_called_once_with(
+      no_proxy_conf.open.assert_called_once_with(
           'http://169.254.169.254/', timeout=1)
 
   def test_get_environment_variable_file(self):
